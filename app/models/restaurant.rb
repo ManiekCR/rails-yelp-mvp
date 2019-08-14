4450ed417e903bcb16480ceabc3501cb39d01068
marian caron <caron.marian@gmail.com>
2019-08-14 11:49:35 +0200

    Restaurant validations added

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,6 @@
 class Restaurant < ApplicationRecord
+  validates :name, uniqueness: true, presence: true
+  validates :address, presence: true
+  validates :category, presence: true, acceptance: { accept:
+    %w[chinese french italian japanese belgian] }
 end
