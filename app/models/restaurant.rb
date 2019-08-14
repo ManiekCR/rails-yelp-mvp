4bfc264ffc5137561990576cd99afea73ef0472c
marian caron <caron.marian@gmail.com>
2019-08-14 11:57:27 +0200

    updated phone_number attribute to string type

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,8 @@
 class Restaurant < ApplicationRecord
   validates :name, uniqueness: true, presence: true
   validates :address, presence: true
+  validates :phone_number, presence: true
   validates :category, presence: true, acceptance: { accept:
     %w[chinese french italian japanese belgian] }
+  has_many :reviews
 end
